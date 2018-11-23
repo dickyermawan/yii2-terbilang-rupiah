eebc05fd4e949f4af9e266bb2705b81ef10d94ca
Dicky Ermawan S <wanasaja@gmail.com>
2018-11-23 19:53:06 +0700

    add fileHeader

--- Rupiah.php
+++ Rupiah.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * @Author: Dicky Ermawan S., S.T., MTA 
+ * @Email: wanasaja@gmail.com 
+ * @Linkedin: linkedin.com/in/dickyermawan 
+ * @Date: 2018-11-23 19:52:47 
+ * @Last Modified by:   Dicky Ermawan S., S.T., MTA 
+ * @Last Modified time: 2018-11-23 19:52:47 
+ */
+
 
 namespace dickyermawan\terbilang;
 
