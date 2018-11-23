9864ea1ce7d9efd383a3b0ea73ecceb9169f97e4
Dicky Ermawan S <wanasaja@gmail.com>
2018-11-23 20:10:33 +0700

    tambah fungsi terbilang

--- Rupiah.php
+++ Rupiah.php
@@ -4,8 +4,8 @@
  * @Email: wanasaja@gmail.com 
  * @Linkedin: linkedin.com/in/dickyermawan 
  * @Date: 2018-11-23 19:52:47 
- * @Last Modified by:   Dicky Ermawan S., S.T., MTA 
- * @Last Modified time: 2018-11-23 19:52:47 
+ * @Last Modified by: Dicky Ermawan S., S.T., MTA
+ * @Last Modified time: 2018-11-23 19:57:45
  */
 
 
@@ -20,4 +20,35 @@ class Rupiah extends \yii\base\Widget
     {
         return "Hello Rupiah!";
     }
+
+    protected function terbilang($nilai)
+    {
+        $huruf = array("", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas");
+        if ($nilai == 0) {
+            return "Kosong";
+        } elseif ($nilai < 12 & $nilai != 0) {
+            return "" . $huruf[$nilai];
+        } elseif ($nilai < 20) {
+            return Terbilang($nilai - 10) . " Belas ";
+        } elseif ($nilai < 100) {
+            return Terbilang($nilai / 10) . " Puluh " . Terbilang($nilai % 10);
+        } elseif ($nilai < 200) {
+            return " Seratus " . Terbilang($nilai - 100);
+        } elseif ($nilai < 1000) {
+            return Terbilang($nilai / 100) . " Ratus " . Terbilang($nilai % 100);
+        } elseif ($nilai < 2000) {
+            return " Seribu " . Terbilang($nilai - 1000);
+        } elseif ($nilai < 1000000) {
+            return Terbilang($nilai / 1000) . " Ribu " . Terbilang($nilai % 1000);
+        } elseif ($nilai < 1000000000) {
+            return Terbilang($nilai / 1000000) . " Juta " . Terbilang($nilai % 1000000);
+        } elseif ($nilai < 1000000000000) {
+            return Terbilang($nilai / 1000000000) . " Milyar " . Terbilang($nilai % 1000000000);
+        } elseif ($nilai < 100000000000000) {
+            return Terbilang($nilai / 1000000000000) . " Trilyun " . Terbilang($nilai % 1000000000000);
+        } elseif ($nilai <= 100000000000000) {
+            return "Maaf Tidak Dapat di Proses Karena Jumlah nilai Terlalu Besar ";
+        }
+    }
+
 }
